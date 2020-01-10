5cdca7ce4816991dcb0e86638c4321d887716288
Marjan Magharehi <marjan.magharehi@gmail.com>
2020-01-10 14:57:48 +0000

    Cleared off product related code, linked search box to api and render results

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,45 @@
 import React, { useState, useEffect } from "react";
 import Recipe from './Recipe';
 
-// SERVICES
-import productService from './services/productService';
-
 function App() {
   const [products, setproducts] = useState(null);
 
-  const APP_KEY = "b2ec0a8f89msh512857a0202d661p1fb6d7jsn92b852513748";
-  // const exampleReq = `https://api.spoonacular.com/recipes/findByIngredients?&ingredients=banana?&apiKey=82974ebaf4d548729e42c9444aa48a55`;
+  const API_KEY = process.env.REACT_APP_RECIPE_API_KEY;
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState(''); 
+  const [query, setQuery] = useState('apple, banana')
 
   useEffect(() => {
     getRecipes();
-    // if(!products) {
-    //   getProducts();
-    }, []);
-  
+    }, [query]);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?&ingredients=banana?&apiKey=82974ebaf4d548729e42c9444aa48a55`)
+    console.log(API_KEY);
+    const response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?&ingredients=${query}?&apiKey=${API_KEY}`)
     const data = await response.json();
     console.log(data);
     setRecipes(data);
   };
 
-  const getProducts = async () => {
-    let res = await productService.getAll();
-    console.log(res);
-    setproducts(res);
-  }
-
-  const renderProduct = product => {
-    return (
-      <li key={product._id} className="list__item product">
-        <h3 className="product__name">{product.name}</h3>
-        <p className="product__description">{product.description}</p>
-      </li>
-    );
+  const updateSearch = e => {
+    setSearch(e.target.value);
   };
 
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+  }
+  
   return (
     <div className="App">
-      <form className="search-form"> 
-        <input className="search-bar" type="text"/>
+      <h1 className="title">Welcome to the product list</h1>
+      <form onSubmit={getSearch} className="search-form"> 
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search-button" type="submit">SEARCH</button>
         {recipes.map((recipe, index) =>(
-          <Recipe title={recipe.title} image={recipe.image}/>
+          <Recipe key={recipe.title} title={recipe.title} image={recipe.image} ingredients={recipe.usedIngredients} />
         ))}
       </form>
-
-
-
-      <h1 className="title">Welcome to the product list</h1>
-      <ul className="list">
-        {(products && products.length > 0) ? (
-          products.map(product => renderProduct(product))
-        ) : (
-          <p>No products found</p>
-        )}
-      </ul>
     </div>
   );
 }
--- client/src/Recipe.js
+++ client/src/Recipe.js
@@ -1,10 +1,15 @@
 import React from "react"; 
 
-const Recipe = ({title,image}) => {
+const Recipe = ({title, image, ingredients}) => {
     return(
         <div>
             <h1>{title}</h1>
-            <p>Calories</p>
+            <ol>
+                {ingredients.map(ingredient =>
+                   <li>{ingredient.name}</li> 
+                )}
+            </ol>
+        
             <img src={image} alt=""/>
            
         </div>
