db4dcee194bc9056fe26fd15669b44059064bf30
Sonia Quintiao <soniaquintiao@gmail.com>
2020-01-10 17:09:21 +0000

    Create main app page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as DB} from 'react-router-dom';
 import NavBar from './components/Navbar';
+import Header from './components/Header';
+import Search from './components/Search';
 import './App.css';
 
 // SERVICES
@@ -10,43 +12,13 @@ import './App.css';
 // // import { Router } from "express";
 
 function App() {
-//   const [products, setproducts] = useState(null);
-
-//   useEffect(() => {
-//     if (!products) {
-//       getProducts();
-//     }
-//   })
-
-//   const getProducts = async () => {
-//     let res = await productService.getAll();
-//     console.log(res);
-//     setproducts(res);
-//   }
-
-//   const renderProduct = product => {
-//     return (
-//       <li key={product._id} className="list__item product">
-//         <h3 className="product__name">{product.name}</h3>
-//         <p className="product__description">{product.description}</p>
-//       </li>
-//     );
-//   };
 
   return (
     <DB>
       <div className="App">
         <NavBar/>
-        {/* <h1 className="title">Welcome to the product list</h1>
-        <ul className="list">
-          {(products && products.length > 0) ? (
-            products.map(product => renderProduct(product))
-          ) : (
-              <p>No products found</p>
-            )}
-        </ul> */}
-        <p>Need a sprinkling of inspiration in the kitchen?</p>
-        <p>Let's pepper your thoughts for you!</p>
+        <Header />
+        <Search />
       </div>
     </DB>
   );
---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <p>Need a sprinkling of inspiration in the kitchen?</p>
+      <p>Let me pepper your thoughts!</p>
+    </div>
+  );
+}
+
+export default Header;
