cb385b22f691306572aef3abcecc9907e88ceb49
Sam King <samuelking94@gmail.com>
2020-01-14 13:19:35 +0000

    Implement backend route for external api call
    Functionality validated with postman

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 // import models
 require('./models/product');
@@ -25,6 +26,32 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
+
+app.get('/api/test/:params', (req, res) => {
+  let params = req.params['params'];
+  res.json({test: "a test json response", params: `${params}`});
+});
+
+app.get('/api/recipe/by-ingredients/:ingredients', async (req, res) => {
+  const query = req.params['ingredients'];
+  const API_KEY = process.env.REACT_APP_RECIPE_API_KEY;
+  const response = await fetch(
+    `https://api.spoonacular.com/recipes/complexSearch?query=${query}` +
+      `&sort=popularity` +
+      `&sortDirection=desc` +
+      `&number=10` +
+      `&addRecipeInformation=true` +
+      `&ignorePantry=true` +
+      `&apiKey=${API_KEY}`
+  );
+  const data = await response.json();
+
+  return res.status(200).send({
+    error: false,
+    data
+  })
+});
+
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`)
 });
