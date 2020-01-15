6a2ab7cf1355fb7d0c25e04dbf1e757b4cfdba5e
Sonia Quintiao <soniaquintiao@gmail.com>
2020-01-15 19:39:28 +0000

    Incorporated card and popup into Recipe component

--- client/src/Recipe.js
+++ client/src/Recipe.js
@@ -1,62 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
+} from "reactstrap";
 
-const Recipe = ({ 
-  title, 
-  image, 
-  time, 
-  cuisine, 
-  servings, 
+// const [modal, setModal] = useState(false);
+
+// const toggle = () => setModal(!modal);
+
+const Recipe = ({
+  title,
+  image,
+  time,
+  cuisine,
+  servings,
   wines,
   //wineList, //keep if can fix wine list mapping
-  ingredients, 
+  ingredients,
   instructions,
   sourceUrl,
   sourceName,
-  diets,
+  diets
 }) => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
   return (
     <div>
-      <h1>{title}</h1>
-      <ol>Make this with:</ol>
-
-      <img className={image} src={image} alt="" />
-      <div>Ready in {time} minutes.</div>
-      <div>Serves up to {servings}.</div>
-      <ul>Cuisine: {cuisine.map((cuisine,index) =>
-        <li key={index}>{cuisine}</li>)}
-      </ul>
-
-      <div>
-        <h2>Ingredients:</h2>
-          <ul>{ingredients.map((ingredient,index) =>
-            <li key={index}>{ingredient.amount.toLocaleString()} {ingredient.unitShort} {ingredient.name}</li>
-            )}
-          </ul>
-
-          <ul>{ingredients.map((ingredient,index) =>
-            <span key={index}><img src={ingredient.image} alt=""/></span>
-            )}
-          </ul>
-      </div>
-
-      <div>
-        <h2>Recipe:</h2>
-        <ol>{instructions.map(instruction =>
-          <li key={instruction.step}>{instruction.step}</li>
-            )}
-        </ol>
-      </div>
-
-      <div><h3>Wine recommendations:</h3>{wines}</div>
-      
-      <div>
-        <ul>{diets.map((diet,index) =>
-          <li key={index}>{diet}</li>)}
-        </ul>
-      </div>
-
-      <div>Source: <a href={sourceUrl}>{sourceName}</a></div>
-
+      <Card>
+        <CardImg top width="100%" src={image} alt="Card image caption" />
+        <CardBody>
+          <CardTitle>{title}</CardTitle>
+          <CardSubtitle>Cooking time {time}minutes.</CardSubtitle>
+          <CardText>Serves up to {servings}.</CardText>
+          <CardText>Wine recommendation: {wines}</CardText>
+          <CardText>
+            <a href={sourceUrl}>{sourceName}</a>
+          </CardText>
+          <Button className="recipe-button" color="danger" onClick={toggle}>
+            Season me up!
+          </Button>
+          <Modal isOpen={modal} toggle={toggle}>
+            <ModalHeader toggle={toggle}>{title}</ModalHeader>
+            <ModalBody>
+              {" "}
+              <h1>Ingredients</h1>
+              <ul>
+                {ingredients.map((ingredient, index) => (
+                  <li key={index}>
+                    {ingredient.amount.toLocaleString()} {ingredient.unitShort}{" "}
+                    {ingredient.name}
+                  </li>
+                ))}
+              </ul>
+              <ul>
+                {ingredients.map((ingredient, index) => (
+                  <span key={index}>
+                    <img src={ingredient.image} alt="" />
+                  </span>
+                ))}
+              </ul>
+            </ModalBody>
+            <ModalBody>
+              <h1>Method</h1>
+              <ol>
+                {instructions.map(instruction => (
+                  <li key={instruction.step}>{instruction.step}</li>
+                ))}
+              </ol>
+            </ModalBody>
+            <ModalBody>
+              <h3>Dietary Requirements:</h3>
+              <ul>
+                {diets.map((diet, index) => (
+                  <li key={index}>{diet}</li>
+                ))}
+              </ul>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={toggle}>
+                Save Recipe
+              </Button>
+              <Button color="secondary" onClick={toggle}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </Modal>
+        </CardBody>
+      </Card>
     </div>
   );
 };
