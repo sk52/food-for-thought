0ef484853af4dbb922b52502f2f816fbf0ed2a38
Sam King <samuelking94@gmail.com>
2020-01-15 15:33:26 +0000

    Stub external api call response with json response

--- client/cypress/integration/home_page_spec.js
+++ client/cypress/integration/home_page_spec.js
@@ -1,6 +1,18 @@
+import ApiResponse from "./api_test_response.json";
+
 describe('The Home Page', function() {
-    it('successfully loads', function() {
+it('successfully loads', function() {
       cy.visit('/');
-      cy.get('.title').should('contain', 'Welcome to the product list');
-    })
-})
+      cy.get('.home').should('contain', 'Food For Thought');
+    });
+});
+
+describe('Search', function() {
+  it('Can use submit button', function() {
+    cy.server();
+    cy.route({method: 'GET', url: 'http://localhost:3000/api/recipe/by-ingredients/beef', response: ApiResponse});
+    cy.get('.input').type('beef');
+    cy.get('.search-button').click();
+    cy.get(':nth-child(7) > h1').should('contain', 'TEST DATA - Balsamic Roast Beef French Dip Sandwich');
+  });
+});
