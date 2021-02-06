3c00ee80db8170543c081f9eb8aea32f1ab19d10
Sam King <samuelking94@gmail.com>
2021-02-06 20:26:18 +0000

    framework # resolve security alerts on dependencies

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 
 const app = express();
-
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
 
 app.use(bodyParser.json());
 
