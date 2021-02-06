95dc5bd856bfc35435d37a9891163c15cffb0f8a
Sam King <samuelking94@gmail.com>
2021-02-06 20:26:18 +0000

    framework # resolve security alerts on dependencies

--- client/src/components/Search.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Search from './Search';
-
-test('input has placeholder', () => {
-    const { getByPlaceholderText } = render(<Search />);
-    const placeholder = getByPlaceholderText(/e.g. tomato, cheese, basil/i);
-    expect(placeholder).toBeInTheDocument();
-})
